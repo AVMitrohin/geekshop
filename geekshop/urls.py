41a2ca5cc8c785bd5f9a07981b030abfc7eb5118
Andrey <avmitrohin@gmail.com>
2019-04-09 07:39:40 +0300

    Homework 4

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -16,12 +16,21 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 import mainapp.views as mainapp
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import include
+import authapp.views as authapp
 
 
 urlpatterns = [
-    url(r'^$', mainapp.main),
-    url(r'^products/', mainapp.products),
-    url(r'^contacts/', mainapp.contacts),
+    url(r'^$', mainapp.main, name='index'),
+    url(r'^auth/', include('authapp.urls', namespace='auth')),
+    url(r'^products/', include('mainapp.urls', namespace='products')),
+    url(r'^contacts/', mainapp.contacts, name='contacts'),
     url(r'^admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
+from .models import Product, ProductCategory
+
+
 
 
 def main(request):
     return render(request, 'mainapp/index.html')
 
 
-def products(request):
-    return render(request, 'mainapp/products.html')
+def products(request, pk=None):
+    product_list = Product.objects.all()
+
+    return render(request, 'mainapp/products.html', context={'products': product_list})
 
 
 def contacts(request):
     return render(request, 'mainapp/contacts.html')
-
