9fa111b0a2e08cb8f2acca40a09c0db3d7ff95cc
Andrey <avmitrohin@gmail.com>
2019-04-19 07:16:55 +0300

    'Homework-7'

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     url(r'^contacts/', mainapp.contacts, name='contacts'),
     url(r'^admin/', admin.site.urls),
     url(r'^basket/', include('basketapp.urls', namespace='basket')),
+    url(r'^admin_custom/', include('adminapp.urls', namespace='admin_custom')),
 ]
 
 if settings.DEBUG:
